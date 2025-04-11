acabd698b3e1fb21d511658905f3f1ff34ab2ed6
ehemmete <eric@sneakypockets.com>
2025-04-11 15:04:24 -0500

    Initial Commit

---
+++ AppWithTool/AppWithToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppWithToolApp.swift
+//  AppWithTool
+//
+//  Created by Eric Hemmeter on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AppWithToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
